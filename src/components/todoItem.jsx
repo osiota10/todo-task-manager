dbd95a26e6a522b1372ce04f7757017c3e894d8d
Osiota Samuel Obrozie <osiotaobrozie@gmail.com>
2023-10-08 15:45:01 +0100

    Checkbox State

--- src/components/todoItem.jsx
+++ src/components/todoItem.jsx
@@ -1,12 +1,20 @@
+import { useState } from "react";
+
 const TodoItem = ({ id, task, is_completed }) => {
+    const [completed, setCompleted] = useState(is_completed);
+
+    const handleCheckboxClick = () => {
+        setCompleted(!completed); // Toggle the completed state
+    };
     return (
         <section className="bg-light rounded mb-2 py-3 px-3" key={id}>
             <div class="form-check">
                 <input
                     class="form-check-input"
                     type="checkbox"
-                    defaultChecked={is_completed}
-                    id="flexCheckDefault"
+                    checked={completed}
+                    onChange={handleCheckboxClick} // Use onChange instead of onClick
+                    id={`flexCheckDefault${id}`} // Use a unique ID for each checkbox
                 />
                 <label
                     class={`form-check-label ${
